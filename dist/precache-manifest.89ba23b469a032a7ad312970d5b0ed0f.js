4422d0dc0ad4c302d8dc1a4436eb297ab0d4f89b
Kheops Valencia <kheopsvalencia@gmail.com>
2018-10-17 20:49:38 -0500

    .gitignore is now working

---
+++ dist/precache-manifest.89ba23b469a032a7ad312970d5b0ed0f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "626ef92c976a430305a4",
+    "url": "/js/contactme.8ede5dac.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "af8e8e3bb22bdd2ea049",
+    "url": "/js/contactpopit.6c9de918.js"
+  },
+  {
+    "revision": "f7b0c54ff370ff6b2f98",
+    "url": "/js/chunk-vendors.ac7f5977.js"
+  },
+  {
+    "revision": "7aaa7782d8f3a62a2834",
+    "url": "/js/cardslot.607816bd.js"
+  },
+  {
+    "revision": "450d96972664c2cd4b32",
+    "url": "/js/app.1c70b261.js"
+  },
+  {
+    "revision": "d30129209f9ad58bd780a174c5ebc88d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a161b347d6750b198c25d686c31422ef",
+    "url": "/img/logo_original_web.a161b347.png"
+  },
+  {
+    "revision": "af8e8e3bb22bdd2ea049",
+    "url": "/css/contactpopit.a860af55.css"
+  },
+  {
+    "revision": "626ef92c976a430305a4",
+    "url": "/css/contactme.d192f38a.css"
+  },
+  {
+    "revision": "7aaa7782d8f3a62a2834",
+    "url": "/css/cardslot.a67787ee.css"
+  },
+  {
+    "revision": "450d96972664c2cd4b32",
+    "url": "/css/app.9fb70a24.css"
+  }
+];
